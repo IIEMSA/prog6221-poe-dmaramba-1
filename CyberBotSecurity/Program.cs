9bf7e1d76f684b4fb6f88b68f8b1699ac7e8e4df
dmaramba <dumisani.maramba@gmail.com>
2026-03-03 13:24:21 +0200

    Add welcome message to the user

--- CyberBotSecurity/Program.cs
+++ CyberBotSecurity/Program.cs
@@ -1,4 +1,6 @@
-﻿
-
+﻿Console.WriteLine("Welcome to CyberBot Security!");
+Console.WriteLine("Please enter your name:");
+var userName = Console.ReadLine();
+Console.WriteLine($"Hello, {userName}! Let's get started with securing your cyber environment.");
 
 Console.ReadKey();
