e83cf7a6a705793773eaad0a8d1a90a125b459cd
dmaramba <dumisani.maramba@gmail.com>
2026-03-20 09:15:33 +0200

    added some testing

--- CyberBotSecurity/Program.cs
+++ CyberBotSecurity/Program.cs
@@ -7,15 +7,10 @@ Console.Write("Please enter your name:");
 var userName = Console.ReadLine();
 Console.WriteLine($"Hello, {userName}! Let's get started with securing your cyber environment.");
 
-var chatbot = new ChatbotLogic();
+//var chatbot = new ChatbotLogic();
 var userMessage = "";
 
-//do
-//{
-//    Console.WriteLine("Please enter your message (type 'exit' to quit):");
-//    userMessage = Console.ReadLine();
-//    chatbot.ProcessMessage(userName, userMessage);
-//} while (!userMessage.Equals("exit", StringComparison.OrdinalIgnoreCase));
+
 while (userMessage != "exit")
 {
     Console.WriteLine();
